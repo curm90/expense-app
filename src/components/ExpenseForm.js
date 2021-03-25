de31625bac4a9f5bf886a18a435aefd1e2d55446
Liam Sutton <devliam90@gmail.com>
2021-03-25 23:02:24 +0000

    add date picker

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 
 class ExpenseForm extends React.Component {
   constructor(props) {
@@ -10,6 +12,7 @@ class ExpenseForm extends React.Component {
       note: props.expense ? props.expense.note : '',
       amount: props.expense ? (props.expense.amount / 100).toString() : '',
       createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+      calendarFocused: false,
       error: '',
     };
   }
@@ -27,6 +30,16 @@ class ExpenseForm extends React.Component {
   onAmountChange = (e) => {
     const amount = e.target.value;
     this.setState(() => ({ amount }));
+  };
+
+  onDateChange = (createdAt) => {
+    if (createdAt) {
+      this.setState(() => ({ createdAt }));
+    }
+  };
+
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }));
   };
 
   onSubmit = (e) => {
@@ -64,6 +77,14 @@ class ExpenseForm extends React.Component {
             placeholder='Amount'
             value={this.state.amount}
             onChange={this.onAmountChange}
+          />
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
           />
           <textarea
             placeholder='Add a note for your expense (Optional)'
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import '@csstools/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/initialize';
 
 const store = configureStore();
 
