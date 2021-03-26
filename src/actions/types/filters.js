f2333607e8e578ec71fb071ae98f919bd80102b7
Liam Sutton <devliam90@gmail.com>
2021-03-26 15:45:26 +0000

    test filters actions

--- src/actions/types/filters.js
+++ src/actions/types/filters.js
@@ -1,4 +1,4 @@
-export const SET_TEXT_FILTER = 'SET_TEXT_FILTER ';
+export const SET_TEXT_FILTER = 'SET_TEXT_FILTER';
 export const SORT_BY_DATE = 'SORT_BY_DATE';
 export const SORT_BY_AMOUNT = 'SORT_BY_AMOUNT';
 export const SET_START_DATE = 'SET_START_DATE';
