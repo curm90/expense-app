3d601abc5a27152e8778f4fd58552d70035dd52e
Liam Sutton <devliam90@gmail.com>
2021-04-02 14:06:50 +0100

    set up private route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import Header from '../components/Header';
 import NotFoundPage from '../components/NotFoundPage';
 import HelpPage from '../components/HelpPage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import LoginPage from '../components/LoginPage';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createBrowserHistory();
 
@@ -15,12 +15,11 @@ const AppRouter = () => {
   return (
     <Router history={history}>
       <div>
-        <Header />
         <Switch>
           <Route path='/' component={LoginPage} exact />
-          <Route path='/dashboard' component={ExpenseDashboardPage} />
-          <Route path='/edit/:id' component={EditExpensePage} />
-          <Route path='/create' component={AddExpensePage} />
+          <PrivateRoute path='/dashboard' component={ExpenseDashboardPage} />
+          <PrivateRoute path='/edit/:id' component={EditExpensePage} />
+          <PrivateRoute path='/create' component={AddExpensePage} />
           <Route path='/help' component={HelpPage} />
           <Route component={NotFoundPage} />
         </Switch>
